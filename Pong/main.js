8ace675d4dac7e6b194e06676b5d81a6d7a1758e
alex9123 <101290286+alex9123@users.noreply.github.com>
2022-05-26 13:26:39 -0600

    Add files via upload

--- Pong/main.js
+++ Pong/main.js
@@ -1,30 +1,41 @@
 // Pong
 let cnv = document.getElementById("canvas");
-let ctx = cnv.getContext("2d");
+let ctx = cnv.getContext("2d")
+
+cnv.width = 700
+cnv.height = 500
+
+console.log(cnv.width, cnv.height)
 
 let key = []
 
+let playerSpeed = 7
+
 let player1 = {
     x: 10,
-    y: 10,
-    width: 5,
-    length: 25
+    y: 100,
+    width: 11,
+    length: 70,
+    score: 0,
 }
 
 let player2 = {
-    x: cnv.width - 12,
-    y: 10,
-    width: 5,
-    length: 25
+    x: cnv.width - 21,
+    y: 100,
+    width: 11,
+    length: 70,
+    score: 0,
 }
 
 let ball = {
     x: cnv.width/2 - 5,
     y: cnv.height/2 - 5,
-    velocity: 1,
-    sideLength: 5
+    velocity: 5,
+    sideLength: 12,
+    direction: Math.atan2(0, -10)
 }
 
+let storedBallp = []
 
 function update() {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
@@ -33,25 +44,35 @@ function update() {
     
     ballfunc(); // Ball Movement
 
+    score(); // Draw Score
+
     requestAnimationFrame(update);
 }
 
 function move() {
     
-    if (key[87] && player1.y > 0) { // W
-        player1.y-- 
-    }
+    // if (key[87] && player1.y > 0) { // W
+    //     player1.y -= playerSpeed
+    // }
 
-    if (key[83] && player1.y < cnv.height - player1.length) { // S
-        player1.y++
+    // if (key[83] && player1.y < cnv.height - player1.length) { // S
+    //     player1.y += playerSpeed
+    // }
+
+    if (ball.x < cnv.width/2) {
+        if (Math.round(player1.y/7) * 7 > ball.y && player1.y > 0) {
+            player1.y -= playerSpeed
+        } else if (Math.round(player1.y/7) * 7 < ball.y && player1.y < cnv.height - player1.length) {
+            player1.y += playerSpeed
+        }
     }
 
     if (key[38] && player2.y > 0) { // Arrow Up
-        player2.y-- 
+        player2.y -= playerSpeed 
     }
 
     if (key[40] && player2.y < cnv.height - player2.length) { // Arrow Down
-        player2.y++
+        player2.y += playerSpeed
     }
 
 
@@ -60,51 +81,70 @@ function move() {
     ctx.fillRect(player2.x, player2.y, player2.width, player2.length)
 }
 
+
+// collision and calculate what angle ball bounces depending on where on paddle it hits
 function collision(ball, player) {
+    let unit = 90/player.length
     if (ball.x < player.x + player.width &&
         ball.x + ball.sideLength > player.x &&
         ball.y < player.y + player.length &&
-        ball.sideLength + ball.y > player.y) {
-            if (ball.y < player.y + player.length) {
-                return true
-            }
+        ball.sideLength + ball.y > player.y
+      ) {
+            ball.velocity = 10
+            let ballPos = ball.y + ball.sideLength/2
+            if (ballPos - player.y <= player.length/2) { // top paddle
+                return (360 - (unit * (player.length/2 - (ballPos - player.y)))) * Math.PI/180
+            } else if (ballPos - player.y >= player.length/2) // bottom paddle
+                return (unit * ((ballPos - player.y) - player.length/2)) * Math.PI/180
         }
-
+        
     return false
 }
 
 
 function ballfunc() {
 
-    if (ball.y > cnv.height - ball.sideLength || ball.y < 0) { // If ball hits top and bottom
-        ball.velocityY *= -1
-    }
- 
-    if (collision(ball, player1) || collision(ball, player2)) { // collide with paddle
-        ball.velocityX *= -1
+    
 
-        
+    if (ball.y > cnv.height - ball.sideLength || ball.y < 0) { // If ball hits top and bottom
+        ball.direction *= -1
     }
+    if (collision(ball, player1)) { // collide with player 1 paddle
+        ball.direction = Math.PI - collision(ball, player1) // Math.PI in front to reverse angle
+    } else if (collision(ball, player2)) { // collide with player 2 paddle
+        ball.direction = collision(ball, player2)
+    }
+
 
     if (ball.x > cnv.width - 5) { // Give point to Player 1
         ball.x = cnv.width/2 - ball.sideLength
         ball.y = cnv.height/2 - ball.sideLength
-
-        ball.velocityX *= -1
+        player1.score ++
+        ball.direction = 0
+        ball.velocity = 5
     }
 
     if (ball.x < 0) { // Give point to Player 2
         ball.x = cnv.width/2 - ball.sideLength
         ball.y = cnv.height/2 - ball.sideLength
+        player2.score ++
 
-        ball.velocityX *= -1
+        ball.direction = Math.PI
+        ball.velocity = 5
     }
 
-
-    ball.y += Math.cos(bullets[i].direction) * ball.velocity
-    ball.x += Math.sin(bullets[i].direction) * bullets[i].Bspeed
+    // draw ball
+    ball.x += Math.cos(ball.direction) * -ball.velocity
+    ball.y += Math.sin(ball.direction) * ball.velocity
 
     ctx.fillRect(ball.x, ball.y, ball.sideLength, ball.sideLength)
+}
+
+function score() {
+    ctx.fillStyle = "white"
+    ctx.font = '30px Fantasy';
+    ctx.fillText(player1.score, cnv.width/2 - 20, 35);
+    ctx.fillText(player2.score, cnv.width/2 + 20, 35);
 }
 
 
