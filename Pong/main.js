f8f240e602e70986ef0422a925c50fac59aca53c
alex9123 <101290286+alex9123@users.noreply.github.com>
2022-05-31 13:12:55 -0600

    Add files via upload

--- Pong/main.js
+++ Pong/main.js
@@ -7,7 +7,15 @@ cnv.height = 500
 
 let key = []
 
-let playerSpeed = 7
+let playerSpeed = 8
+let twoPlayer
+
+let paddleSound = document.createElement("audio")
+paddleSound.src = "Sounds/PaddleSound.mp3"
+let wallSound = document.createElement("audio")
+wallSound.src = "Sounds/WallSound.mp3"
+let scoreSound = document.createElement("audio")
+scoreSound.src = "Sounds/ScoreSound.mp3"
 
 let player1 = {
     x: 10,
@@ -49,33 +57,35 @@ function update() {
 
 function move() {
     
-    // if (key[87] && player1.y > 0) { // W
-    //     player1.y -= playerSpeed
-    // }
-
-    // if (key[83] && player1.y < cnv.height - player1.length) { // S
-    //     player1.y += playerSpeed
-    // }
-
-
-    // Decide which side of paddle to hit ball pased on oponent position
-    let paddleSide 
-    if (player2.y > cnv.height/2) { 
-        paddleSide = player1.length
-    } else {
-        paddleSide = 0
-    }
-    // Round values so paddle does not look weird
-    let player1p = Math.round((player1.y + paddleSide)/playerSpeed) * playerSpeed
-    let ballp = Math.round((ball.y + ball.sideLength/2)/playerSpeed) * playerSpeed
-    
-    if (ball.x < cnv.width/2) { // if player is past halfway point
-        if (player1p > ballp && player1.y > 0) { // if ball above player
+    if (twoPlayer) { // two players
+        if (key[87] && player1.y > 0) { // W
             player1.y -= playerSpeed
-        } else if (player1p < ballp && player1.y < cnv.height - player1.length) { // if ball is below
+        }
+
+        if (key[83] && player1.y < cnv.height - player1.length) { // S
             player1.y += playerSpeed
         }
+    } else { // 1 player and 1 AI
+        // Decide which side of paddle to hit ball pased on oponent position
+        let paddleSide 
+        if (player2.y > cnv.height/2) { 
+            paddleSide = 0
+        } else {
+            paddleSide = player1.length
+        }
+        // Round values so paddle does not look weird
+        let player1p = Math.round((player1.y + paddleSide)/playerSpeed) * playerSpeed
+        let ballp = Math.round((ball.y + ball.sideLength/2)/playerSpeed) * playerSpeed
+        
+        if (ball.x < cnv.width/2) { // if player is past halfway point
+            if (player1p > ballp && player1.y > 0) { // if ball above player
+                player1.y -= playerSpeed
+            } else if (player1p < ballp && player1.y < cnv.height - player1.length) { // if ball is below
+                player1.y += playerSpeed
+            }
+        }
     }
+
 
     if (key[38] && player2.y > 0) { // Arrow Up
         player2.y -= playerSpeed 
@@ -100,6 +110,7 @@ function collision(ball, player) {
         ball.y < player.y + player.length &&
         ball.sideLength + ball.y > player.y
       ) {
+            paddleSound.play()
             ball.velocity = 12
             let ballPos = ball.y + ball.sideLength/2
             if (ballPos - player.y <= player.length/2) { // top paddle
@@ -111,19 +122,23 @@ function collision(ball, player) {
     return false
 }
 
+let coolDown = 0 
+function reflect() { // cooldown to bounce so ball doesn't clip into sides
+    if (coolDown < 1) {
+        wallSound.play()
+        ball.direction = 2 * Math.PI - ball.direction
+        coolDown = 1
+    }
+
+    setTimeout(() => {
+        coolDown --
+    }, 100);
+}
+
 function ballfunc() {
-    if (ball.y + ball.sideLength > cnv.height || ball.y < 0) { // If ball hits top and bottom
-        ball.direction = 2 * Math.PI - ball.direction  
-    }
-    
-    if (collision(ball, player1)) { // collide with player 1 paddle
-        ball.direction = Math.PI - collision(ball, player1) // Math.PI in front to reverse angle
-    } else if (collision(ball, player2)) { // collide with player 2 paddle
-        ball.direction = collision(ball, player2)
-    }
-
-
+ 
     if (ball.x > cnv.width - 5) { // Give point to Player 1
+        scoreSound.play()
         ball.x = cnv.width/2 - ball.sideLength
         ball.y = cnv.height/2 - ball.sideLength
         player1.score ++
@@ -132,6 +147,7 @@ function ballfunc() {
     }
 
     if (ball.x < 0) { // Give point to Player 2
+        scoreSound.play()
         ball.x = cnv.width/2 - ball.sideLength
         ball.y = cnv.height/2 - ball.sideLength
         player2.score ++
@@ -140,19 +156,28 @@ function ballfunc() {
         ball.velocity = 6
     }
 
-    // draw ball
+    if (collision(ball, player1)) { // collide with player 1 paddle
+        ball.direction = Math.PI - collision(ball, player1) // Math.PI in front to reverse angle
+    } else if (collision(ball, player2)) { // collide with player 2 paddle
+        ball.direction = collision(ball, player2)
+    } else if (ball.y + ball.sideLength > cnv.height || ball.y < 0) { // If ball hits top and bottom
+        reflect()
+    }
+
+   
     ball.x += Math.cos(ball.direction) * -ball.velocity
     ball.y += Math.sin(ball.direction) * ball.velocity
     
 
+  
     ctx.fillRect(ball.x, ball.y, ball.sideLength, ball.sideLength)
 }
 
 function score() {
     ctx.fillStyle = "white"
     ctx.font = '30px Fantasy';
-    ctx.fillText(player1.score, cnv.width/2 - 20, 35);
-    ctx.fillText(player2.score, cnv.width/2 + 20, 35);
+    ctx.fillText(player1.score, cnv.width/2 - 30, 35);
+    ctx.fillText(player2.score, cnv.width/2 + 30, 35);
 }
 
 
@@ -167,4 +192,39 @@ function keyupHandler(event) {
     key[event.keyCode] = false;
 }
 
-requestAnimationFrame(update);
+// startup screen
+let buttonX = cnv.width/2 - 62
+let buttonY = 115
+let buttonW = 125
+let buttonH = 50
+
+document.addEventListener("click", clickDetector)
+
+function clickDetector(event) {
+    let cnvRect = cnv.getBoundingClientRect();
+    scaleX = canvas.width / cnvRect.width,    
+    scaleY = canvas.height / cnvRect.height;
+
+    mx = (event.x - cnvRect.x) * scaleX;
+    my = (event.y - cnvRect.y) * scaleY;
+
+    if(mx>=buttonX && mx<=buttonX+buttonW && my>=buttonY && my<=buttonY+buttonH){
+        twoPlayer = false
+        requestAnimationFrame(update)
+    } else if (mx>=buttonX && mx<=buttonX+buttonW && my>=buttonY+100 && my<=buttonY+buttonH+100) {
+        twoPlayer = true
+        requestAnimationFrame(update)
+    }
+}
+
+
+ctx.fillStyle = "white"
+ctx.strokeStyle = "white"
+ctx.font = '30px Fantasy';
+ctx.fillText("Pong", cnv.width/2 - 35, 35);
+ctx.fillText("1 Player", cnv.width/2 - 47, 150);
+ctx.strokeRect(buttonX, buttonY, buttonW, buttonH)
+ctx.fillText("2 Players", cnv.width/2 - 55, 250);
+ctx.strokeRect(buttonX, buttonY + 100, buttonW, buttonH)
+
+
